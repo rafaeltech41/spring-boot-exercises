446aae259c4cf785ff2333faefd14e6fe76e8e5e
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2025-03-14 17:17:30 -0300

    feat: Criação do projeto Spring Boot e configuração inicial

---
+++ testeSpring/src/test/java/com/senai/testeSpring/TesteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.testeSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
